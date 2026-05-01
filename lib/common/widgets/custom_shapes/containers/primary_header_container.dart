9a8e46dc962aa01c8d726b6033ae353b5c9f5e1b
gem66888-ui <gem66888@gmail.com>
2026-05-01 04:25:22 +0100

    feat: implement vault screen with custom primary header container and add flutter_svg dependency

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:lockin/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:lockin/common/widgets/custom_shapes/containers/curved_edges_widget.dart';
+import 'package:lockin/constants/colors.dart';
+
+class PrimaryHeaderContainer extends StatelessWidget {
+  const PrimaryHeaderContainer({
+    super.key,
+    required this.isdark,
+    required this.child,
+  });
+
+  final bool isdark;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return UiCurvedEdgesWidget(
+      child: Container(
+        color: isdark ? UiColors.darkGrey10 : UiColors.darkGrey20,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(width: double.infinity, height: 280, child: child),
+      ),
+    );
+  }
+}
--- lib/features/vault/presentation/pages/vault_screen.dart
+++ lib/features/vault/presentation/pages/vault_screen.dart
@@ -1,16 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:lockin/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:lockin/constants/colors.dart';
-import 'package:lockin/features/vault/presentation/widgets/vault_appbar.dart';
+import 'package:lockin/constants/sizes.dart';
+import 'package:lockin/utils/helpers/helper_functions.dart';
 
 class VaultScreen extends StatelessWidget {
   const VaultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDark = HelperFunctions.isDarkMode(context);
     return Scaffold(
-      backgroundColor: UiColors.darkGrey20,
       body: SafeArea(
-        child: SingleChildScrollView(child: Column(children: [VaultAppbar()])),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              PrimaryHeaderContainer(
+                isdark: isDark,
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                      child: Container(
+                        color: UiColors.primaryBackground,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        height: 100,
+                        width: double.infinity,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(UiSizes.md),
+                                  decoration: BoxDecoration(
+                                    color: UiColors.grey,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  child: Text(
+                                    "FE",
+                                    style: Theme.of(
+                                      context,
+                                    ).textTheme.headlineSmall,
+                                  ),
+                                ),
+                                const SizedBox(width: UiSizes.md),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Franklin Emmanuel",
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.headlineSmall,
+                                    ),
+                                    Text(
+                                      "Welcome back",
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.bodyMedium,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.menu,
+                                color: isDark ? UiColors.white : UiColors.dark,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Store, manage and secure all your password in one secure vault.",
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: UiSizes.spaceBtwSections),
+
+              const SizedBox(height: UiSizes.spaceBtwItems),
+            ],
+          ),
+        ),
       ),
     );
   }
